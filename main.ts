c509b0243c58667d1633c2ea879c2dba69c17b73
Juictaceg <Justacegahunia1@gmail.com>
2024-12-16 19:40:11 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+    wuKong.stopMotor(wuKong.MotorList.M2)
+})
+basic.forever(function () {
+	
+})
